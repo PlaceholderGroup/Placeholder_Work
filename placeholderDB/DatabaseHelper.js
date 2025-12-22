b252a22882f258f7607f5cf946466b78680efa3b
Jack Detrick <98991308+JackDetrick66@users.noreply.github.com>
2025-12-22 11:03:46 -0500

    Update DatabaseHelper.js

--- placeholderDB/DatabaseHelper.js
+++ placeholderDB/DatabaseHelper.js
@@ -1,2 +1,21 @@
-import sqlite3 from 'sqlite3';
+import SQLite from 'react-native-sqlite-storage';
 
+
+SQLite.enablePromise(true);
+
+
+class DBHelper {
+    constructor(){
+        this.db = null;
+    }
+
+
+    async initDB() {
+        this.db = await SQLite.openDatabase({
+            name: 'contactsDB.db',
+            location:'default'
+        });
+        console.log('Database opened successfully!');
+    }
+
+}
