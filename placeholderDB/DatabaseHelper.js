4ed932485a7748f58afb6d5b2c8e861ea85658df
Jack Detrick <98991308+JackDetrick66@users.noreply.github.com>
2025-12-30 21:54:00 -0500

    Update DatabaseHelper.js

--- placeholderDB/DatabaseHelper.js
+++ placeholderDB/DatabaseHelper.js
@@ -11,11 +11,36 @@ class DBHelper {
 
 
     async initDB() {
+        // await = pause while this happens
+        try{ 
         this.db = await SQLite.openDatabase({
             name: 'contactsDB.db',
             location:'default'
         });
         console.log('Database opened successfully!');
+
+        // create necessary tables
+        await this.createTables(); //need to make this still
+
+        } catch(error){
+        console.log('Issue with opening the database.');
+        throw(error);
+        }
+    }
+
+    async createTables(){
+        // create the tables we need
+        const queries = [
+            `CREATE TABLE IF NOT EXISTS users (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                username TEXT UNIQUE NOT NULL,
+                password_hash TEXT NOT NULL,
+                created_at TEXT DEFAULT CURRENT_TIMESTAMP,
+                photo REAL
+        );`
+
+        ];
+
     }
 
 }
