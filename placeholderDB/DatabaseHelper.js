ea1e3e206ef8323ca5e50e6bfc635573b01a5aab
Jack Detrick <98991308+JackDetrick66@users.noreply.github.com>
2026-01-08 09:21:50 -0500

    Update DatabaseHelper.js

--- placeholderDB/DatabaseHelper.js
+++ placeholderDB/DatabaseHelper.js
@@ -36,10 +36,74 @@ class DBHelper {
                 username TEXT UNIQUE NOT NULL,
                 password_hash TEXT NOT NULL,
                 created_at TEXT DEFAULT CURRENT_TIMESTAMP,
-                photo REAL
+                profile_image_path TEXT
+        );`, // profileImage path to wherever its stored on phone
+            `CREATE TABLE IF NOT EXISTS profiles (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                personal_profile INTEGER DEFAULT 0,
+                name TEXT UNIQUE NOT NULL,
+                image TEXT
+        );`,
+            `CREATE TABLE IF NOT EXISTS contact_fields (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                profile_id INTEGER NOT NULL,
+                field_type TEXT NOT NULL,
+                field_value TEXT NOT NULL,
+                FOREIGN KEY (profile_id) REFERENCES profiles(id)
         );`
-
         ];
+        
+        try{
+            for (const query of queries){
+                await this.db.executeSql(query);
+            }
+        }catch(error){
+            console.log('Error when creating tables: ', error);
+            throw error;
+        }
+
+    }
+
+    async createNewUser(username, password_hash){
+
+    }
+    
+    async createProfile(username, image = null){
+        const query = `INSERT INTO profiles (name, image) VALUES (?, ?)`;
+
+        try{
+            const result = await this.db.executeSQL(query, [username, image]);
+        } catch(error) {
+            console.log('Error when creating profile: ', error);
+            throw error;
+        }
+    }
+    //delete the profile and related contact fields
+    async deleteProfile(username){
+
+    }
+
+    async updateProfile(username){
+
+    }
+
+    async getProfile(username){
+
+    }
+
+    async addContactField(profileID, field_type, field_value){
+
+    }
+
+    async editContactField(profileID, field_type, field_value){
+
+    }
+
+    async getContactFields(profileID, field_type = null){
+
+    }
+
+    async deleteContactField(){
 
     }
 
